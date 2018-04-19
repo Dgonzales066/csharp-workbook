ec50c9da5cad22086b1413ea7881083146eb02a6
David Gonzales <dgonzales033@satx.rr.com>
2018-04-19 15:14:23 -0500

    rev to pig lating app

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -11,19 +11,37 @@ namespace PigLatin
 
             Console.WriteLine("Lets write in Pig Latin.");
             Console.WriteLine("Enter a word or a sentence.");
-            Console.WriteLine();
+            Console.WriteLine("");//blank space
             string enterWords = Console.ReadLine();
-            Console.WriteLine(TranslateWord(enterWords));
+            Console.WriteLine(LowerCaseWords (TranslateWord(enterWords)));
+            Console.WriteLine("");//blank Space
         }
 
         public static string TranslateWord(string firstWord)
         {
-            int firstVowelIndex = firstWord.IndexOfAny(new char[] {'a', 'e', 'i', 'o', 'u'});
+            int firstVowelIndex = firstWord.IndexOfAny(new char[] {'A', 'E', 'I', 'O', 'U','a', 'e', 'i', 'o', 'u'});
 
             string firstLetter = firstWord.Substring(0, firstVowelIndex);
             string newWord = firstWord.Substring(firstVowelIndex);
-            return newWord + firstLetter + "ay";
+
+            if (firstVowelIndex == 0)
+            {
+                return newWord + "yay";
+            }
+            else
+            {
+                return newWord + firstLetter + "ay";
+            }
+
         }
+
+        public static string LowerCaseWords(string newWord)
+        {
+            return newWord.ToLower();
+        }
+        
+        //return newWord + firstLetter + "ay";
+        //return firstWord.IndexOfAny(new char[] {'a', 'e', 'i', 'o', 'u'});
 
         
     }
