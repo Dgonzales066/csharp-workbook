a41f3ebc2003fdc460bcef9d38ed1c67cfcc6083
David Gonzales <dgonzales033@satx.rr.com>
2018-04-22 23:16:42 -0500

    Rock, paper, scissors game done. Included all combinations and created an new game dialog

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -4,14 +4,33 @@ namespace RockPaperScissors
 {
     class Program
     {
+        
         public static void Main()
         {
-            Console.WriteLine("Enter hand 1:");
-            string hand1 = Console.ReadLine().ToLower();
-            Console.WriteLine("Enter hand 2:");
-            string hand2 = Console.ReadLine().ToLower();
-            Console.WriteLine(CompareHands(hand1, hand2));
+            bool keepPlaying = true;
+            while(keepPlaying)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Choose between rock, paper, or scissors.");
+                Console.WriteLine("Enter hand 1:");
+                string hand1 = Console.ReadLine().ToLower();
+                Console.WriteLine("Enter hand 2:");
+                string hand2 = Console.ReadLine().ToLower();
+                Console.WriteLine();
+                Console.WriteLine(CompareHands(hand1, hand2));
 
+                Console.WriteLine("New game? [yes or no]");
+                string answer = "";
+                answer = Console.ReadLine();
+                if (answer == "yes")
+                {
+                    keepPlaying = true;
+                }
+                else
+                {
+                    keepPlaying = false;
+                }
+            }
             // leave this command at the end so your program does not close automatically
             //Console.ReadLine();
         }
@@ -19,24 +38,96 @@ namespace RockPaperScissors
         public static string CompareHands(string hand1, string hand2)
         {
             // Your code here
-            //hand1 = "rock";
-            //if (hand2 == "scissors")
-            //{
-            //    Console.WriteLine("Hand One Wins!");
-            //}
-            //else if (hand2 == "paper")
-            //{
-            //    Console.WriteLine("Hand Two Wins!");
-            //}
-            switch (hand1 == "rock")
+            string playerOne = hand1;
+            string playerTwo = hand2;
+
+            if (playerOne == "rock")
             {
-            case (hand2 = "scissors")
-                return ("Hand One Wins!");
+                if (playerTwo == "rock")
+                {
+                    Console.WriteLine("It's a tie.");
+                }
+                else if (playerTwo == "paper")
+                {
+                    Console.WriteLine("You lose.");
+                }
+                else 
+                {
+                    Console.WriteLine("You win!");
+                }
             }
+            else if (playerOne == "paper")
+            {
+                if (playerTwo == "rock")
+                {
+                    Console.WriteLine("You win!");
+                }
+                else if (playerTwo == "paper")
+                {
+                    Console.WriteLine("It's a tie");
+                }
+                else 
+                {
+                    Console.WriteLine("You lose.");
+                }
+            }
+            else //(playerOne == "scissors")
+            {
+                if (playerTwo == "rock")
+                {
+                    Console.WriteLine("You lose.");
+                }
+                else if (playerTwo == "paper")
+                {
+                    Console.WriteLine("You win.");
+                }
+                else 
+                {
+                    Console.WriteLine("It's a tie.");
+                }
+            }
+            return playerOne + " vs. " + playerTwo;
 
+            // string playerOne = "rock";
 
+            // switch (playerOne)
+            // {
+            //     case "rock":
+            //         Console.WriteLine("Its a tie.");
+            //         break;
 
-            return hand1 + " " + hand2;
+            //     case "paper":
+            //         Console.WriteLine("You lose");
+            //         break;
+
+            //     default :
+            //         Console.WriteLine("You win");
+            //         break;
+            // }
+
+            // // string playerOne = "";
+            // // string playerTwo = (playerOne == "rock") ? "its a tie." : "you lose.";
+            // // Console.WriteLine("Player One {0}", playerOne);
+            // //playerOne = (playerTwo == "paper") ? "you lose." : "you win." ;
+            // //hand1 = (hand2 == "scissors") ? "you win";
+            
+            // //hand1 = "rock";
+            // //if (hand2 == "scissors")
+            // //{
+            // //    Console.WriteLine("Hand One Wins!");
+            // //}
+            // //else if (hand2 == "paper")
+            // //{
+            // //    Console.WriteLine("Hand Two Wins!");
+            // //}
+            // // switch (hand1 == "rock")
+            // // {
+            // // case (hand2 = "scissors")
+            // //     return ("Hand One Wins!");
+            // // }
+
+            // //return playerOne + " " + playerTwo;
+            //return " ";
         }
     }
 }
