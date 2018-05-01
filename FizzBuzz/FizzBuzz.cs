5f17acbde0544f5242981c702cff0c0aacc41999
David Gonzales <dgonzales033@satx.rr.com>
2018-05-01 08:09:46 -0500

    Fizz Buzz Logic created

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -6,7 +6,26 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //List numbers 1 to 100;
+            for (int i = 1; i < 101; i++)
+            {   
+                if (i % 15 == 0)//numbers divisible by 3 and 5 print FizzBuzz
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)//number divisible by 3 print Fizz
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)//number divisible by 5 pirnt Buzz
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);//number not divisible by either 3 or 5
+                }
+            }
         }
     }
 }
