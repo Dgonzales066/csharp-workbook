ac9cb8135a79f694c021566c5b35c422ad7b0bd3
David Gonzales <dgonzales033@satx.rr.com>
2018-05-03 21:11:16 -0500

    garage game and OOP class creation

--- OOP/OOP.cs
+++ OOP/OOP.cs
@@ -6,7 +6,42 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
+
+    class Car
+    {
+        public Car(string initialColor)
+        {
+             color = initialColor;
+        }
+
+        public string color { get; set; }
+        
+    }
+
+    class Garage
+    {
+        private Car[] cars;
+
+        public Garage(int size)
+        {
+            Size = size;
+            this.cars = new Car[size];
+        }
+
+        public int Size { get; private set; }
+
+        public string Cars
+        {
+            get
+            {
+                return $"Car in spot 0 is {cars[0].color}";
+            }
+        }
+    }
+
+
+
 }
