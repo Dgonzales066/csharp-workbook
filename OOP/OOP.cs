54c56b2b8da88f2fe5fcaa9f6fc51980a67fa56d
David Gonzales <dgonzales033@satx.rr.com>
2018-05-08 20:31:54 -0500

    new files and modified file

--- OOP/OOP.cs
+++ OOP/OOP.cs
@@ -4,42 +4,70 @@ namespace OOP
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            
+            Garage smallGarage = new Garage(2);
+            Car redCar = new Car("red");
+            Car blueCar = new Car("blue");
+
+            //smallGarage.ParkCar(carTwo, 0);
+            // Console.WriteLine(Car.Garage.color.Cars());
+
+            Console.WriteLine(smallGarage.Cars);
+            // Console.WriteLine(carOne.color);
+            // Console.WriteLine(smallGarage.cars[spot]);
         }
     }
 
     class Car
     {
+        //Constructor - Initializer?
         public Car(string initialColor)
         {
              color = initialColor;
         }
 
-        public string color { get; set; }
-        
+        //1st Property?
+        public string color { get; }
     }
 
     class Garage
     {
         private Car[] cars;
 
+        //Constructor
         public Garage(int size)
         {
             Size = size;
+            //instantiate the Car with the correct number of spots
             this.cars = new Car[size];
         }
 
+        //Property
         public int Size { get; private set; }
 
+        //Method?
         public string Cars
         {
             get
             {
-                return $"Car in spot 0 is {cars[0].color}";
+                return "Car in spot 1 is {cars[0].color} and the Car in spot 2 is {cars[1].color}";
             }
         }
+
+        public void ParkCar(Car car, int spot)
+        {
+            cars[spot] = car;
+        }
+    }
+
+    class People
+    {
+        public People()
+        {
+
+        }
+
     }
 
 
