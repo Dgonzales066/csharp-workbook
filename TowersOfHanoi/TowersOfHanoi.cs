b0b6a4a4b3f928bd9aae323baaf5656a83484041
David Gonzales <dgonzales033@satx.rr.com>
2018-05-23 21:09:41 -0500

    towers of hanoi game

--- TowersOfHanoi/TowersOfHanoi.cs
+++ TowersOfHanoi/TowersOfHanoi.cs
@@ -6,7 +6,25 @@ namespace TowersOfHanoi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the number of Disk.");
+            int n = 4;
+
+            towerOfHanoi(n, 'A', 'C', 'B');
+            //Console.WriteLine("Enter the number of Disk.");
         }
+
+        static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine("Move disk 1 from rod " + from_rod + " to rod " + to_rod);
+                return;
+            }
+            towerOfHanoi(n-1, from_rod, aux_rod, to_rod);
+            Console.WriteLine("Move disk " + n + " from rod " + from_rod + " to rod " + to_rod);
+            towerOfHanoi(n-1, aux_rod, to_rod, from_rod);
+        }
+
     }
-}
+
+    
+}//End of TowersOfHanoi
