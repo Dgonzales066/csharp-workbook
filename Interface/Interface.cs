8096f30ebab9d53ca8b7e090f70b3acd819ee304
David Gonzales <dgonzales033@satx.rr.com>
2018-05-27 22:34:13 -0500

    added 3rd type of activity to the list

--- Interface/Interface.cs
+++ Interface/Interface.cs
@@ -1,12 +1,145 @@
 ﻿using System;
+using System.Collections.Generic;
+
+// <summary>
+// Using an interface called IActivity a.k.a contract so that we can use it as a type instead of a concrete class type
+// </summary>
 
 namespace Interface
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        // Fields
+        static Engine _engine;
+        static Workflow _workflow;
+
+        // Properties
+        // n/a
+
+        // Constructor(s)
+        static Program()
         {
-            Console.WriteLine("Hello World!");
+            _engine = new Engine();
+            _workflow = new Workflow();
         }
+
+        // Methods
+        public static void Main()
+        {
+            _engine.Run(_workflow);
+        }
+    }
+
+    public class Engine
+    {
+        // Fields
+        // n/a
+
+        // Properties
+        // n/a
+
+        // Constructor(s)
+        public Engine()
+        {
+            // not used
+        }
+
+        // Methods
+        public void Run(Workflow workflow)
+        {
+            foreach (var activity in workflow.GetActivities())
+            {
+                activity.Execute();
+            }
+        }
+    }
+
+    public class Workflow
+    {
+        // Fields
+        List<IActivity> _activities;
+
+        // Properties
+        // n/a
+
+        // Constructor
+        public Workflow()
+        {
+            _activities = new List<IActivity>
+            {
+                { new SportActivity{Message = "Running..." } },
+                { new SportActivity{Message = "Batting..." } },
+                { new SportActivity{Message = "Kicking..." } },
+                { new SportActivity{Message = "Dribbling..." } },
+                { new OutdoorActivity{Message = "Fishing..." } },
+                { new OutdoorActivity{Message = "Camping..." } },
+                { new OutdoorActivity{Message = "Grilling..." } },
+                { new IndoorActivity{Message = "Taking a nap..."} },
+                { new IndoorActivity{Message = "Watching TV..."} },
+                { new IndoorActivity{Message = "Cooking..."} },                
+            };
+        }
+
+        // Methods
+        public List<IActivity> GetActivities()
+        {
+            return _activities;
+        }
+    }
+
+    public class SportActivity : IActivity
+    {
+        // Fields
+        // n/a
+
+        // Properties
+        public string Message { get; set; }
+
+        // Constructor(s)
+        public SportActivity()
+        {
+            // not used
+        }
+
+        // Methods
+        public void Execute()
+        {
+            Console.WriteLine("Performing Sport Activity: {0}", Message);
+        }
+    }
+
+    public class OutdoorActivity : IActivity
+    {
+        public string Message { get; set; }
+
+        public void Execute()
+        {
+            Console.WriteLine("Performing Outdoor Activity: {0}", Message);
+        }
+    }
+
+    public class IndoorActivity : IActivity
+    {
+        public string Message { get; set; }
+
+        public IndoorActivity()
+        {
+
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("Performing Indoor Activity: {0}", Message);
+        }
+    }
+
+    //Here is an example of an interface
+    public interface IActivity
+    {
+        // Properties
+        string Message { get; set; }
+
+        // Methods
+        void Execute();
     }
 }
